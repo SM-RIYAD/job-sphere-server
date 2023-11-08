0949a5746379b27e673d352634faaff0f2ee5180
SM RIYAD <smriyad0185@gmail.com>
2023-11-08 18:19:44 +0600

    jwt function is implemented

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -7,44 +9,147 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wacbf1n.mongodb.net/?retryWrites=true&w=majority`;
-const uri=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wacbf1n.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wacbf1n.mongodb.net/?retryWrites=true&w=majority`;
 // middleware
-app.use(cors());
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173","https://job-sphere.web.app","https://job-sphere.firebaseapp.com"],
+    credentials: true,
+  })
+);
+const logger = (req, res, next) => {
+  console.log("log: info", req.method, req.url);
+  next();
+};
 app.use(express.json());
 
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-  
-  const dbConnect = async () => {
-    try {
-      await client.connect();
-      console.log("Database Connected!");
-    } catch (error) {
-      console.log(error.name, error.message);
+app.use(cookieParser());
+
+///verify token
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  // no token available
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
     }
-  };
-  dbConnect();
-  const allJobCollection = client.db("jobsphere").collection("alljobs");
-  const appliedJobCollection = client.db("jobsphere").collection("appliedjobs");
-  app.get("/alljobs", async (req, res) => {
-    const cursor = allJobCollection.find();
-    const result = await cursor.toArray();
-    res.send(result);
+    req.user = decoded;
+    next();
   });
-  ///adding job api
+};
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log("Database Connected!");
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+};
+dbConnect();
+const allJobCollection = client.db("jobsphere").collection("alljobs");
+const appliedJobCollection = client.db("jobsphere").collection("appliedjobs");
+app.get("/alljobs", async (req, res) => {
+  const cursor = allJobCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+///auth related api
+app.post("/jwt",logger, async (req, res) => {
+  const user = req.body;
+  console.log("user for token after login", user);
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
+ res.cookie(
+    "token",
+    token,
+    {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true: false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    }
+    )
+    // .cookie("token", token, {
+    //   httpOnly: true,
+    //   secure: true,
+    //   sameSite: "none",
+    // })
+    .send({ success: true });
+});
+
+// res.cookie(
+//   "token",
+//   tokenValue,
+//   {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production" ? true: false,
+//   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+//   }
+//   )
+  // Removing/Deleting cookie:
+  // res.clearCookie(
+  // "token",
+  // {
+  // maxAge: 0,
+  // secure: process.env.NODE_ENV === "production" ? true: false,
+  // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  // }
+  // )
+
+
+
+// res.cookie('token', token, {
+//   httpOnly: true,
+//   secure: true,
+//   sameSite: 'none'
+// })
+
+////LOG OUT
+app.post("/logout", async (req, res) => {
+  const user = req.body;
+  console.log("logging out", user);
+
+  // clearCookie('token', { maxAge: 0 }).
+  res.clearCookie(
+    "token",
+    {
+    maxAge: 0,
+    secure: process.env.NODE_ENV === "production" ? true: false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    }
+    ).send({ success: true })
+
+  // res.clearCookie("token", {
+  //   maxAge: 0,
+  //   secure: process.env.NODE_ENV === "production" ? true : false,
+  //   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  // });
+});
+
+///adding job api
 app.post("/addjob", async (req, res) => {
   const newjob = req.body;
   console.log(newjob);
   const result = await allJobCollection.insertOne(newjob);
   res.send(result);
 });
- ///applying job api
- app.post("/applyjob", async (req, res) => {
+///applying job api
+app.post("/applyjob", async (req, res) => {
   const newjobtoApply = req.body;
   console.log(newjobtoApply);
   const result = await appliedJobCollection.insertOne(newjobtoApply);
@@ -62,19 +167,36 @@ app.get("/specificjob/:id", async (req, res) => {
 });
 ///my job api
 
-app.get('/myjobs',async (req, res) => {
+app.get("/myjobs",verifyToken, async (req, res) => {
   console.log(req.query.email);
-  // console.log('token owner info', req.user)
-  // if(req.user.email !== req.query.email){
-  //     return res.status(403).send({message: 'forbidden access'})
-  // }
+  console.log("token owner info", req.user);
+  if (req.user.email !== req.query.email) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  console.log("cookies test", req.cookies);
   let query = {};
   if (req.query?.email) {
-      query = { useremail: req.query.email }
+    query = { useremail: req.query.email };
   }
   const result = await allJobCollection.find(query).toArray();
   res.send(result);
-})
+});
+///my applied jobs api
+
+app.get("/myappliedjobs", logger,verifyToken, async (req, res) => {
+  console.log("cookies test", req.cookies);
+  console.log(req.query.email);
+  console.log("token owner info", req.user);
+  if (req.user.email !== req.query.email) {
+    return res.status(403).send({ message: "forbidden access" });
+  }
+  let query = {};
+  if (req.query?.email) {
+    query = { applicantEmail: req.query.email };
+  }
+  const result = await appliedJobCollection.find(query).toArray();
+  res.send(result);
+});
 
 ///update a job api
 app.put("/updatejob/:id", async (req, res) => {
@@ -82,63 +204,54 @@ app.put("/updatejob/:id", async (req, res) => {
   const filter = { _id: new ObjectId(id) };
   const options = { upsert: true };
   const newjobtoUpdate = req.body;
-  console.log("from body update", newjobtoUpdate );
+  console.log("from body update", newjobtoUpdate);
   const newjob = {
-    // name: updatedProduct.name,
-    // brand: updatedProduct.brand,
-    // date: updatedProduct.date,
-    // price: updatedProduct.price,
-    // rating: updatedProduct.rating,
-    // description: updatedProduct.description,
-    // type: updatedProduct.type,
-    // photo: updatedProduct.photo,
+    companyLogo: newjobtoUpdate.companyLogo,
 
-    companyLogo:newjobtoUpdate.companyLogo,
-        
-    jobPhoto:newjobtoUpdate.jobPhoto,
+    jobPhoto: newjobtoUpdate.jobPhoto,
 
-    JobTitle:newjobtoUpdate.JobTitle,
+    JobTitle: newjobtoUpdate.JobTitle,
 
-    UserName:newjobtoUpdate.UserName,
-    Location:newjobtoUpdate.Location,
-    useremail:newjobtoUpdate.useremail,
+    UserName: newjobtoUpdate.UserName,
+    Location: newjobtoUpdate.Location,
+    useremail: newjobtoUpdate.useremail,
 
-    JobCategory:newjobtoUpdate.JobCategory,
-    CompanyName:newjobtoUpdate.CompanyName,
-    SalaryRange:newjobtoUpdate.SalaryRange,
+    JobCategory: newjobtoUpdate.JobCategory,
+    CompanyName: newjobtoUpdate.CompanyName,
+    SalaryRange: newjobtoUpdate.SalaryRange,
 
-    JobDescription:newjobtoUpdate. JobDescription,
+    JobDescription: newjobtoUpdate.JobDescription,
 
-    JobPostingDate:newjobtoUpdate.JobPostingDate,
+    JobPostingDate: newjobtoUpdate.JobPostingDate,
 
-    ApplicationDeadline:newjobtoUpdate. ApplicationDeadline,
+    ApplicationDeadline: newjobtoUpdate.ApplicationDeadline,
 
-    JobApplicantsNumber:newjobtoUpdate.JobApplicantsNumber,
+    JobApplicantsNumber: newjobtoUpdate.JobApplicantsNumber,
   };
-  console.log("new job",  newjob);
+  console.log("new job", newjob);
   const job = {
     $set: {
-      companyLogo:newjobtoUpdate.companyLogo,
-        
-      jobPhoto:newjobtoUpdate.jobPhoto,
-  
-      JobTitle:newjobtoUpdate.JobTitle,
-  
-      UserName:newjobtoUpdate.UserName,
-      Location:newjobtoUpdate.Location,
-      useremail:newjobtoUpdate.useremail,
-  
-      JobCategory:newjobtoUpdate.JobCategory,
-      CompanyName:newjobtoUpdate.CompanyName,
-      SalaryRange:newjobtoUpdate.SalaryRange,
-  
-      JobDescription:newjobtoUpdate. JobDescription,
-  
-      JobPostingDate:newjobtoUpdate.JobPostingDate,
-  
-      ApplicationDeadline:newjobtoUpdate. ApplicationDeadline,
-  
-      JobApplicantsNumber:newjobtoUpdate.JobApplicantsNumber,
+      companyLogo: newjobtoUpdate.companyLogo,
+
+      jobPhoto: newjobtoUpdate.jobPhoto,
+
+      JobTitle: newjobtoUpdate.JobTitle,
+
+      UserName: newjobtoUpdate.UserName,
+      Location: newjobtoUpdate.Location,
+      useremail: newjobtoUpdate.useremail,
+
+      JobCategory: newjobtoUpdate.JobCategory,
+      CompanyName: newjobtoUpdate.CompanyName,
+      SalaryRange: newjobtoUpdate.SalaryRange,
+
+      JobDescription: newjobtoUpdate.JobDescription,
+
+      JobPostingDate: newjobtoUpdate.JobPostingDate,
+
+      ApplicationDeadline: newjobtoUpdate.ApplicationDeadline,
+
+      JobApplicantsNumber: newjobtoUpdate.JobApplicantsNumber,
     },
   };
 
@@ -147,33 +260,38 @@ app.put("/updatejob/:id", async (req, res) => {
   res.send(result);
 });
 
-
-
-
 /// update applicants number api
 
-app.get('/updateapplicantnumber/:id', async (req, res) => {
+app.get("/updateapplicantnumber/:id", async (req, res) => {
   const id = req.params.id;
   const filter = { _id: new ObjectId(id) };
   // const jobdata = req.body;
   // console.log(jobdata);
   const updatedjobdata = {
-      $inc: {
-          // status: updatedBooking.status
-          JobApplicantsNumber: 1
-      },
+    $inc: {
+      // status: updatedBooking.status
+      JobApplicantsNumber: 1,
+    },
   };
   const result = await allJobCollection.updateOne(filter, updatedjobdata);
-  console.log( "result from update", result )
+  console.log("result from update", result);
   res.send(result);
-})
+});
+
+///delete a job api
+app.delete("/deletejob/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await allJobCollection.deleteOne(query);
+  console.log(result);
+  res.send(result);
+});
 app.get("/", (req, res) => {
-    res.send("job sphere server is running");
-  });
-  
+  res.send("job sphere server is running");
+});
+
 app.listen(port, () => {
-    console.log(
-      `job sphere Server is running on port: ${port}, ${process.env.DB_USER},${process.env.DB_PASS} `
-    );
-  });
-  
+  console.log(
+    `job sphere Server is running on port: ${port}, ${process.env.DB_USER},${process.env.DB_PASS} `
+  );
+});
